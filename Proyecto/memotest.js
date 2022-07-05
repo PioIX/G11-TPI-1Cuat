aca25fc61f4c2621c065945f61e4e1996c05736f
fedsalga <fedesalgadoirala@pioix.edu.ar>
2022-07-05 11:50:03 -0300

    incompleto
    tengo que hacer el back fede

---
+++ Proyecto/memotest.js
@@ -0,0 +1,48 @@
+function imagen(id){
+
+    numero = document.getElementById(id).id
+    console.log(numero)
+    document.getElementById(numero).innerHTML = `
+    <img src="img/ODS1.png" width="80" height="80" >
+    ` 
+    
+}
+
+
+
+
+
+function destapar(numero){
+    
+    document.getElementById(numero)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
