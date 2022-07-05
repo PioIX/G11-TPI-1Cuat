201ccfd6e51daf35d23e144062fb931288ab4f82
IanOzafran <101183244+dbottini2022pioix@users.noreply.github.com>
2022-07-05 11:52:44 -0300

    codycross v1.2

---
+++ Proyecto/codycross/codycross.js
@@ -0,0 +1,12 @@
+var palabraVertical = "";
+var listaPalabrasHorizontales = [];
+// funcion para obtener una nueva palabra vertical
+function nuevaPalabraVertical() {
+    return false;
+}
+
+// funcion para obtener una nueva lista de palabras horizontales
+function nuevaListaPalabrasHorizontales() {
+    return false;
+}
+
